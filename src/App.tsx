c24d9af357fc27cbd0a6786b9fa07451efa68406
Vladosik337 <vlad.ostapenko.337@gmail.com>
2024-10-30 22:17:59 +0200

    create project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/style.scss';
+import Keyboard from "./components/Keyboard.tsx";
+
+
+function App() {
+
+  return (
+    <div className="min-h-full flex flex-col items-center justify-center overflow-clip h-screen">
+        <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
